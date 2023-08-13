d517c5e038237305c91a6631378b3ee2ca7581e1
betelhem <betelhemkirub@gmail.com>
2023-08-13 23:07:12 +0300

    added diffrent mock up

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -22,19 +22,15 @@ const About = () => {
         </div>
 
       </div>
-      <div className="flex flex-col gap-5 rounded-[30px] py-20 px-16 text-secondary-100 mt-32 bg-gradient-to-b from-pink-200 to-pink-100 ">
+      <div className="flex flex-col gap-5 rounded-[30px] py-20 px-16 text-secondary-100 mt-32 bg-pink-200 ">
         <h1 className=" font-inter font-semibold text-4xl ">About Me</h1>
         <p className="text-lg font-inter ">
           I am currently in my fourth year pursuing a degree in Software Engineering at AASTU. Throughout my academic journey, I have consistently exhibited a strong inclination towards understanding the underlying principles and methodologies behind the design and production of various systems and products. This inclination, coupled with my curiosity in the process and production aspect, has sparked my keen interest in front-end development.
 
 As I delved deeper into the realm of software development, I realized that simply fulfilling user or business requirements was not enough. I yearned to bridge the gap between abstract ideas and tangible designs. This realization propelled me towards the captivating field of UI/UX design, where I could actively participate in transforming user or business requirements into visually appealing and user-friendly interfaces.
 
-By exploring UI/UX design, I aim to leverage my software engineering background to create intuitive and seamless user experiences. I recognize the significance of aligning user needs with business goals, thereby ensuring the success of any digital product. This involves empathizing with users, conducting thorough research, and employing design thinking methodologies to craft meaningful and engaging interfaces.
 
-          <p className='pt-3'>
             
-In summary, my journey towards becoming a UI/UX designer stems from my deep-rooted curiosity about the how and why behind the design and production processes. By combining my knowledge of software engineering with the art of UI/UX design, I aim to create visually appealing and user-centric interfaces that fulfill both user needs and business objectives.
-          </p>
           
         </p>
       </div>
--- src/component/Work.jsx
+++ src/component/Work.jsx
@@ -3,8 +3,8 @@ import Hero from './Hero'
 import Desk from '../Image/desktop.svg'
 import AyatLogo from "../Image/Ayat-Logo.svg"
 import OutletLogo from "../Image/Outlet-Logo.svg"
-import Outlet from "../Image/Outlet.png"
-import Ayat from "../Image/Ayat.png"
+import Outlet from "../Image/Frame 124.png"
+import Ayat from "../Image/Frame 128.png"
 import Omnifood from "../Image/Omnifood.png"
 import OmnifoodLogo from "../Image/omnifood-logo.png"
 
@@ -31,19 +31,19 @@ const Work = () => {
         <div className="w-44">
           <img src={OutletLogo} alt="" />
         </div>
-            <p className='text-2xl tracking-wide font-thin'> 2023-UI-Design-UX-Design-Web Development</p>
+            <p className='text-xl tracking-wide font-thin'> 2023-UI-Design-UX-Design-Web Development</p>
         
 
-          <div className="flex items-start justify-start gap-10  flex-col">
+          <div className="flex items-start justify-start gap-8  flex-col">
           <div className="flex items-start justify-start gap-3   flex-col">
             <h1 className='font-inter font-semibold text-4xl'>Outlet Store</h1>
-            <p className='text-2xl w-3/4 '>Creating an e-commerce website that is easy to use and navigate is essential for a Store that wants to sell products online.</p>
+            <p className='text-2xl  '>Creating an e-commerce website that is easy to use and navigate is essential for a Store that wants to sell products online.</p>
               </div> 
-          <a href='https://outlet-store-e-commerce.netlify.app/' className=' hover:bg-secondary-100/90 transition-all duration-300 text-2xl px-12 rounded-[10px] py-4 bg-secondary-100/70 text-Outlet-1 tracking-wide font-inter capitalize '>View site</a>
+          <a href='https://outlet-store-e-commerce.netlify.app/' className=' hover:bg-secondary-100/90 transition-all duration-300 text-xl px-8 rounded-[8px] py-3 bg-secondary-100/70 text-Outlet-1 tracking-wide font-inter capitalize '>View site</a>
           </div>
             </div>
             
-          <div className="w-[70%] group-hover:scale-[1.2]  transition-all duration-500 ">
+          <div className="w-[130%] group-hover:scale-[1.2]  transition-all duration-500 ">
             <img src={Outlet} alt="" />
           </div>
           
@@ -57,19 +57,19 @@ const Work = () => {
         <div className="w-44">
           <img src={AyatLogo} alt="" />
         </div>
-            <p className='text-2xl text- tracking-wide font-thin'> 2023-UI-Design-UX-Design-Web Development</p>
+            <p className='text-xl text- tracking-wide font-thin'> 2023-UI-Design-UX-Design-Web Development</p>
         
 
-          <div className="flex items-start justify-start gap-10 flex-col">
+          <div className="flex items-start justify-start gap-8 flex-col">
           <div className="flex items-start justify-start gap-3 text-Ayat-1.2  flex-col">
             <h1 className='font-inter text-4xl font-semibold'>Ayat  Realstate Redesign</h1>
-            <p className='text-2xl w-3/4'>Redesign Ayat Real Estate's website to make it more  visually appealing and easy to use.</p>
+            <p className='text-2xl '>Redesign Ayat Real Estate's website to make it more  visually appealing and easy to use.</p>
               </div> 
-          <a href='https://ayat-realstate.netlify.app/' className='hover:bg-Ayat-1.2/90  transition-all duration-300 text-2xl px-12  rounded-[10px] py-4 bg-Ayat-1.2/70 text-secondary-100 tracking-wide font-inter capitalize '>View site</a>
+          <a href='https://ayat-realstate.netlify.app/' className='hover:bg-Ayat-1.2/90  transition-all duration-300 text-xl px-8  rounded-[8px] py-3 bg-Ayat-1.2/70 text-secondary-100 tracking-wide font-inter capitalize '>View site</a>
               </div>
               </div>
           
-        <div className="w-[70%] group-hover:scale-[1.2] transition-all duration-500">
+        <div className="w-[120%] group-hover:scale-[1.2] pt-10  transition-all duration-500">
             <img src={Ayat} alt="" />
           </div>
           
@@ -83,15 +83,15 @@ const Work = () => {
         <div className="w-96 pt-10">
           <img src={OmnifoodLogo} alt="" />
         </div>
-            <p className='text-2xl text-Omnifood-1.2 tracking-wide font-thin'> 2023-UI-Design-UX-Design-Web Development</p>
+            <p className='text-xl text-Omnifood-1.2 tracking-wide font-thin'> 2023-UI-Design-UX-Design-Web Development</p>
         
 
           <div className="flex items-start justify-start gap-10 e flex-col">
           <div className="flex items-start justify-start gap-3 text-secondary-200 tracking-wide flex-col">
             <h1 className='font-inter font-semibold text-4xl'>Omni Food</h1>
-            <p className='text-2xl w-3/4 '>A seamless service that provides healthy, pre-made meals that are tailored to your individual dietary needs and preferences.</p>
+            <p className='text-2xl w-4/5 '>A seamless service that provides healthy, pre-made meals that are tailored to your individual dietary needs and preferences.</p>
                 </div> 
-          <a href='https://omnifood-beti.netlify.app/' className='hover:bg-Omnifood-1.2/90  transition-all duration-300 text-2xl px-12 rounded-[10px] py-4 bg-Omnifood-1.2/70 text-secondary-100 tracking-wide font-inter capitalize '>View site</a>
+          <a href='https://omnifood-beti.netlify.app/' className='hover:bg-Omnifood-1.2/90  transition-all duration-300 text-xl px-8 rounded-[8px] py-3 bg-Omnifood-1.2/70 text-secondary-100 tracking-wide font-inter capitalize '>View site</a>
                 </div>
           </div>
           
